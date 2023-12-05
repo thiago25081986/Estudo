f6439ec61cc83a46878a4ec8ee54ce0ede08596c
thiago25081986 <141791842+thiago25081986@users.noreply.github.com>
2023-12-05 14:53:50 -0300

    Add files via upload

---
+++ aula_21_b_teste_conhecimento.py
@@ -0,0 +1,13 @@
+Variavel_email=input("Digite seu e-mail:"          )
+variavel_senha= input("Digite a sua senha: ")
+
+email_permitido= 'thiseixas300@gmail.com'
+senha_permitida= ('1234')
+
+if Variavel_email==email_permitido and variavel_senha==senha_permitida:
+    print("acesso permitido")
+
+else:
+    print("acesso negado")
+
+
